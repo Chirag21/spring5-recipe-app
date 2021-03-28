a8e2ff8403c7def503bd16f9210a1a7da198af06
Chirag21 <cp.chirag21@gmail.com>
2021-03-28 22:34:38 +0530

    Updated JUnit 4 to JUnit5.

--- src/test/java/com/spring5/recipe/repositories/UnitOfMeasureRepositoryIntegrationTest.java
+++ src/test/java/com/spring5/recipe/repositories/UnitOfMeasureRepositoryIntegrationTest.java
@@ -4,15 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.spring5.recipe.domain.UnitOfMeasure;
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 class UnitOfMeasureRepositoryIntegrationTest {
 
@@ -25,7 +22,8 @@ class UnitOfMeasureRepositoryIntegrationTest {
 	}
 
 	@Test
-	//@DirtiesContext	reloads context after done. Used when method changes data in the context
+	// @DirtiesContext reloads context after done. Used when method changes data in
+	// the context
 	void testFindByDescription() {
 		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
 		assertEquals("Teaspoon", uomOptional.get().getDescription());
